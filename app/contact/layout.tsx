322297f78c20eb04e0ca99312a76be8cc7291613
Ogawa Shun <syun.ogawa0609@gmail.com>
2025-03-19 20:33:12 +0900

    コンタクト

---
+++ app/contact/layout.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/app/_compornents/Hero";
+import Sheet from "@/app/_compornents/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="Contact" sub="お問い合わせ" />
+      <Sheet>{children}</Sheet>
+    </>
+  );
+}
